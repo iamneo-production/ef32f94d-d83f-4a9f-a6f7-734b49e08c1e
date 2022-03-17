d8e396d7a9e3cab836f243f9a854326342de7ea1
ASHKKARSIDHIK <ashkkarsidhik.777@gmail.com>
2022-03-17 05:11:58 +0000

    initial commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,13 +1,44 @@
 import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        MEDCARE
-      </header>
-    </div>
+      <div  id="signupBox">
+        <div>
+            <h1>SIGNUP</h1>
+              <div id="email">
+                <input type="text" placeholder="Email" className="name"/>
+              </div>
+
+              <div id="username">
+                <input type="text" placeholder="Username" className="name"/>
+              </div>
+
+              <div id="mobilenumber">
+                <input type="number" placeholder="Mobilenumber" className="name"/>
+              </div>
+
+              <div className="first-input" id="password">  
+                <input type="password" placeholder="Password" className="name"/>
+              </div>
+
+              <div className="second-input" id="password">  
+                <input type="password" placeholder="Confirm Password" className="name"/>
+              </div>
+
+              <div className="login-button" >
+                <button id="submitButton">Signup</button>
+              </div>
+              
+                <p className="link">
+                  <div id="signinLink">
+                    <span>Already have an account ? </span><a href="#">login</a>
+                  </div>    
+                </p> 
+          
+        </div>
+      </div>
   );
 }
-
 export default App;
