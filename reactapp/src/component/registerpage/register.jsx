eab94c2a7f3cc9b4fae13fa6c520e8e79131ca2f
ASHKKARSIDHIK <ashkkarsidhik.777@gmail.com>
2022-03-17 08:45:07 +0000

    initial commit

---
+++ reactapp/src/component/registerpage/register.jsx
@@ -0,0 +1,46 @@
+import './register.css';
+import React from 'react';
+
+
+function Register() {
+  return (
+    <body>
+      <div  id="signupBox">
+        <div>
+            <h1>SIGNUP</h1>
+              <div id="email">
+                <input type="text" placeholder="Email" className="name"/>
+              </div>
+
+              <div id="username">
+                <input type="text" placeholder="Username" className="name"/>
+              </div>
+
+              <div id="mobilenumber">
+                <input type="number" placeholder="Mobilenumber" className="name"/>
+              </div>
+
+              <div className="first-input" id="password">  
+                <input type="password" placeholder="Password" className="name"/>
+              </div>
+
+              <div className="second-input" id="password">  
+                <input type="password" placeholder="Confirm Password" className="name"/>
+              </div>
+
+              <div className="login-button" >
+                <button id="submitButton">Signup</button>
+              </div>
+              
+                <p className="link">
+                  <div id="signinLink">
+                    <span>Already have an account ? </span><a href="#">login</a>
+                  </div>    
+                </p> 
+          
+        </div>
+      </div>
+    </body>
+  );
+}
+export default Register;
